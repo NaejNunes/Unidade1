e4b93d619edb479a599aca8aaef75c6db3c94b72
NaejNunes <jeannobreganunes@hotmail.com>
2020-11-04 09:16:31 -0300

    Conhecendo as variaveis e como mostrar informacoes na tela

---
+++ Projeto1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//RECADO
+
+int main()
+{
+    //VARIÁVEL - espaço em memória quecontém
+    //UM TIPO - NOME - VALOR
+
+    int a = 1; //valores númerico sem pontos - QUANTIDADE - IDADE
+    float b = 2.2 ; //valores númericos com pontos - MONETÁRIO $ - PESO- ALTURA
+    char nome[] = "Jean Carlos da Nóbrega Nunes";
+    printf("a=%d     b=%f      c=%s ",a,b,nome);
+    return 0;
+}
