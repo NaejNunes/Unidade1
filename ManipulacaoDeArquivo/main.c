b8e1fc3e512918b4270795ecc9fb9ea0e65d7c4f
NaejNunes <jeannobreganunes@hotmail.com>
2020-11-05 09:48:54 -0300

    Manipulacao de arquivos em C

---
+++ ManipulacaoDeArquivo/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   FILE *arq;
+   char buffer[50];
+   int i;
+
+   printf("Escrita: \n\n");
+
+   if(!(arq = fopen("C:\\Users\\JeanN\\Documents\\RepositoriosCC\\arq1.txt", "w")))
+    printf("Error! \n");
+   else
+    {
+        sprintf(buffer, "Numeros de 1 a 10 \n");
+        fputs(buffer, arq);
+        for(i = 1; i <=10; i++)
+            fprintf(arq, "%d \n", i);
+        printf("Sucesso! \n\n");
+    }
+    fclose(arq);
+
+    printf("Leitura: \n\n");
+
+    char *titulo[40];
+    int numeros[10];
+
+    if(!(arq = fopen("C:\\Users\\JeanN\\Documents\\RepositoriosCC\\arq1.txt", "r")))
+        printf("Erro! \n");
+    else
+    {
+        fgets(&titulo, 40, arq);
+        printf("Titulo: %s", titulo);
+        for(i=0; i<10; i++)
+        {
+            fscanf(arq, "%d", &numeros[i]);
+            printf("%d \n ", numeros[i]);
+        }
+    }
+    fclose(arq);
+}
