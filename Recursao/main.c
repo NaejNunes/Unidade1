de30e0d47b2c0c51e208086052397c21fcfd98d8
NaejNunes <jeannobreganunes@hotmail.com>
2020-11-05 09:53:22 -0300

    Sistema de recursao em c

---
+++ Recursao/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+  int fatorial(int n)
+   {
+        if(n == 0)
+            return 1;
+        else
+            return n * fatorial(n-1);
+   }
+
+int main()
+{
+   int resultado;
+   resultado = fatorial(5);
+   printf("Fatorial de 5 eh: %d", resultado);
+   return 0;
+}
